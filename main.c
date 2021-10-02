444f802e402a3a7757a7bdd8b6ccefbd97ff88fb
chazzox <42206116+chazzox@users.noreply.github.com>
2021-10-02 17:26:38 +0100

    endlessly prints time

--- main.c
+++ main.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 #include <time.h>
+#include <unistd.h>
 
 int main() {
-    printf("hey\n");
-    time_t t = time(NULL);
-    struct tm tm = *localtime(&t);
-    printf("now: %d-%02d-%02d %02d:%02d:%02d\n", tm.tm_year + 1900,
-           tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec);
+    while (1) {
+        time_t t = time(NULL);
+        struct tm tm = *localtime(&t);
+        printf("now: %02d:%02d:%02d\n", tm.tm_hour, tm.tm_min, tm.tm_sec);
+    }
 }
