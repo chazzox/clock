d77b38b3ff23f806ee9188b6bc89f21354c3f062
chazzox <42206116+chazzox@users.noreply.github.com>
2021-10-16 19:13:41 +0100

    working clock, prints on same line

--- main.c
+++ main.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <time.h>
-#include <unistd.h>
 
 int main() {
     while (1) {
-        time_t t = time(NULL);
-        struct tm tm = *localtime(&t);
-        printf("now: %02d:%02d:%02d\n", tm.tm_hour, tm.tm_min, tm.tm_sec);
+        time_t last = time(NULL);
+        while (time(NULL) == last);
+        struct tm tm = *localtime(&last);
+        printf("\r%02d-%02d-%02d", tm.tm_hour, tm.tm_min, tm.tm_sec);
+        fflush(stdout);
     }
 }
